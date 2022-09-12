fa5c3a4b960a1f8d98f7b4e698bbeb19fd6e0098
Asadullah <97753630+Asadullah850@users.noreply.github.com>
2022-09-12 21:56:45 +0600

    1st commit

---
+++ js.js
@@ -0,0 +1,14 @@
+
+function submit() {
+    let email = document.getElementById("email");
+    let pass = document.getElementById("pass");
+    if (email == "admin") {
+        alert("Saccess");
+      } 
+      else if (pass = 1) {
+        alert("Saccess");
+      } 
+      else{
+        alert("Try agin");
+    }
+}
